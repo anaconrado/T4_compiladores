8314ec7f26de826a9b762433cef6f54c5684d252
Ana <anaconradof@gmail.com>
2024-08-11 14:22:21 -0300

    commit inicial

---
+++ src/main/java/br/ufscar/dc/compiladores/algumasemanticot4/AlgumaSemanticoT4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.ufscar.dc.compiladores.algumasemanticot4;
+
+/**
+ *
+ * @author ana
+ */
+public class AlgumaSemanticoT4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
