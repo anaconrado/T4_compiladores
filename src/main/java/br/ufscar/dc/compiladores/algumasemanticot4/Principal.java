519e5174ae79d7d298bb61b6e4b577f932e05f7d
Ana <anaconradof@gmail.com>
2024-08-11 14:31:24 -0300

    Config do pom

--- src/main/java/br/ufscar/dc/compiladores/algumasemanticot4/Principal.java
+++ src/main/java/br/ufscar/dc/compiladores/algumasemanticot4/Principal.java
@@ -1,15 +1,14 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package br.ufscar.dc.compiladores.algumasemanticot4;
 
 /**
  *
  * @author ana
  */
-public class AlgumaSemanticoT4 {
-
+public class Principal {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
